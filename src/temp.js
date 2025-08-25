8f4a8d389267a623bebc79587c87b6052df2cdf7
zandile-m <zandilemakhapela@gmail.com>
2025-08-25 19:27:12 +0200

    Updated temp.js

--- src/temp.js
+++ src/temp.js
@@ -1,11 +1,36 @@
 function refreshWeather(response){
   let temperatureElement = document.querySelector("#tempVal");  
   temperatureElement.innerHTML = response.data.temperature.current;
-
   let cityElement = document.querySelector("#current");
+    let descriptionElement = document.querySelector("#condText");
+  let humidityElement = document.querySelector("#humVal");
+  let windElement = document.querySelector("#windVal");
+    let timeElement = document.querySelector("#time");
+  let  date= new Date(response.data.time * 1000);
+
+  timeElement.innerHTML = formatDate(date);
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed} km/h`;
+  
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   
+}
+
+function formatDate(date){
+ 
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+ let day = days[date.getDay()];
+
+ if (minutes < 10){
+  minutes = `0${minutes}`;
+ }
+
+ return`${day} ${hours}:${minutes}`;
+
 }
 
 

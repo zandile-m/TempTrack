94e8cefe97b7566c786b3a4f71636735310df089
zandile-m <zandilemakhapela@gmail.com>
2025-08-28 21:14:14 +0200

    Updated the temp.js

--- src/temp.js
+++ src/temp.js
@@ -7,6 +7,9 @@ function refreshWeather(response){
   let windElement = document.querySelector("#windVal");
     let timeElement = document.querySelector("#time");
   let  date= new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+  
+iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
 
   timeElement.innerHTML = formatDate(date);
   cityElement.innerHTML = response.data.city;
